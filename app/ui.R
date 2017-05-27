edb630dcaf4d49aa7e9429f5df05f38fda6a8686
vale-salvatelli <vale.salvatelli@gmail.com>
2017-05-27 16:50:24 +0200

    Plots in cols

--- app/ui.R
+++ app/ui.R
@@ -33,19 +33,26 @@ dashboardBody(
                         )
                ),
                tabPanel("WorldCloud",
-                        sidebarLayout(
-                          sidebarPanel(
-                            textInput("text", label = h3("Text input"), value = "Enter text...")
-                            #selectInput("select", label = h3("Select box"), 
-                             #  choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3)
-                        ),
+                       # sidebarLayout(
+                       #   sidebarPanel(
+                       #     textInput("text", label = h3("Text input"), value = "Enter text...")
+                       
+                      #  ),
                         mainPanel(
-                          plotOutput("plot2")
+                        fluidRow(		
+                        	column(12,
+                        				 sidebarPanel(
+                        				 	textInput("text", label = h3("Text input"), value = "Enter text...")
+                        				 )       
+                        	),
+                        	column(12,plotOutput("plot2", width="500px", height="400px"))
                         )
+                        
                )
                )
     )
 )
-                )
+)
+             
 
 
