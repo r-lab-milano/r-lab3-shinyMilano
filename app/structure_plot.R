2fef7006ae8031ee0e4d19376d163a370115d770
giackgamba <giackgamba@gmail.com>
2017-05-30 01:09:14 +0200

    Plot 'struttura', general ui and server fixes and tidy

---
+++ app/structure_plot.R
@@ -0,0 +1,64 @@
+structure_plot <- function(df, annoInput) {
+  
+  df %>% 
+    filter(anno == annoInput,
+           rendiconto != 0) %>% 
+    ggplot(aes(x = factor(ds_livello3), 
+               y = factor(ds_cdc), 
+               fill = log10(rendiconto))) +
+    geom_tile() +
+    facet_grid(. ~ ds_livello1 + ds_livello2, 
+               scales = 'free',
+               space = 'free',
+               labeller = label_wrap_gen(10)) +
+    scale_fill_distiller('Rendiconto', 
+                         palette = 'Reds', 
+                         direction = 1, 
+                         guide = 'legend',
+                         breaks = 1:6,
+                         labels = c('10 €', '100 €', '1.000 €', '10.000 €', '100.000 €', '1Mil €')
+                         ) +
+    scale_x_discrete('', labels = function(x) str_wrap(x, width = 25), position = 'top') +
+    scale_y_discrete('Centro di costo', labels = function(x) str_wrap(x, width = 25)) +
+    ggtitle('Distribuzione del rendiconto') +
+    guides(size = NULL) +
+    theme_light() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1),
+          strip.placement = 'outside'
+    )
+}
+
+
+## NOT WORKING
+# click_output_rendered <- function(click_pos, df, annoInput) {
+#   
+#   lev1_pos <- click_pos$panelvar1
+#   lev2_pos <- click_pos$panelvar2
+#   lev3_pos <- round(click_pos$x)
+#   cdc_pos <- round(click_pos$y)
+#   
+#   res <- df %>% 
+#     filter(
+#       anno == annoInput, 
+#       rendiconto != 0) %>% 
+#     arrange(desc(factor(ds_livello3))) %>% 
+#     mutate(lev3 = group_indices_(., .dots = 'livello3')) %>% 
+#     filter(lev3 == lev3_pos) %>% 
+#     arrange(desc(factor(ds_cdc))) %>% 
+#     mutate(cdc_group = group_indices_(., .dots = 'cdc')) %>% 
+#     filter(cdc_group == cdc_pos) %>%
+#     select(ds_livello1, ds_livello2, ds_livello3, ds_cdc, rendiconto) %>% 
+#     filter(ds_livello1 == lev1_pos,
+#            ds_livello2 == lev2_pos) %>% 
+#   group_by(ds_livello1, ds_livello2, ds_livello3, ds_cdc) %>% 
+#     summarise(rendiconto = sum(rendiconto)) %>% 
+#     as.list()
+#   
+#   list(lev1 = paste0('Livello 1: ', res$ds_livello1),
+#        lev2 = paste0('Livello 2: ', res$ds_livello2),
+#        lev3 = paste0('Livello 3: ', res$ds_livello3),
+#        cdc = paste0('Centro di Costo:', res$ds_cdc),
+#        rend = paste0('Rendiconto: ', res$rendiconto)
+#   )
+#   
+# }
--- app/ui.R
+++ app/ui.R
@@ -1,66 +1,61 @@
 source("global.R")
 
 dashboardPage(
-	skin = "red",
+  skin = "red",
   dashboardHeader(title = "r-lab3-shinyMilano",
-  								titleWidth = 450),
+                  titleWidth = 450),
   dashboardSidebar(
-    selectInput("tipo", label = h3("Select box"), 
-                choices = c("Tutto", tipo),
-                selectize = TRUE),
-    renderSelectOutput("pdc_descrizione_missione"),
-    renderSelectOutput("pdc_descrizione_programma"),
-    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard"),
-      menuItem("Raw data", tabName = "rawdata")
-    )
+    selectInput("tipo", 
+                label = h3("Select box"), 
+                choices = tipo, selected = 'USCITE'),
+    selectInput('missione', 'Missione', choices = 'Tutto'),
+    selectInput('programma', 'Programma', choices = 'Tutto')
+    
+    # sidebarMenu(
+    #   menuItem("Dashboard", tabName = "dashboard"),
+    #   menuItem("Raw data", tabName = "rawdata")
+    # )
   ),
-
-dashboardBody(
+  
+  dashboardBody(
     navbarPage("Schede",
                tabPanel("Proporzioni",
-                        #sidebarLayout(
-                         # sidebarPanel(
-                         # 	selectInput("var", label = "Grouping Variable",
-                         #  							choices = c("`PDC-Descrizione Livello1`","`PDC-Descrizione Livello2`",
-                         #  													"`PDC-Descrizione Livello3`","`PDC-Descrizione Livello4`"),
-                         #  							selected = "PDC-Descrizione Livello1")
-                         #    ),
-                				 mainPanel(
-                				 	column(12,
-                				 				 selectInput("var", label = "Grouping Variable",
-                				 				 						choices = c("`PDC-Descrizione Livello1`","`PDC-Descrizione Livello2`",
-                				 				 												"`PDC-Descrizione Livello3`","`PDC-Descrizione Livello4`"),
-                				 				 						selected = "PDC-Descrizione Livello1")),
-                				 	plotOutput("proporzione")
-                         ))
-               ,
+                        
+                        div(style="display: inline-block;vertical-align:top; width: 100px;",
+                              selectInput('anno', 'Anno', choices = c(2013:2016), selected = 2016)),
+                        div(style="display: inline-block;vertical-align:top; width: 500px;",
+                              box('All\'interno di ciascun programma selezionato a fianco, 
+                            viene mostrato dove e quanto viene speso per ciascun livello, 
+                            per ciascun centro di costo', width = 12)
+                          ),
+                        plotOutput('structure', click = 'plot_click')
+               ),
                tabPanel("TimeSeries",
-                        fluidPage(
-                          mainPanel(
-                            selectInput("TimeSeries1Choice", "Serie da visualizzare", 
-                                      TimeSeries1PossibleValues),
-                            plotOutput("TimeSeries1")
-                          )
-                        )
-                      ),
+                        # fluidPage(
+                        #   mainPanel(
+                        selectInput("TimeSeries1Choice", "Serie da visualizzare",
+                                    TimeSeries1PossibleValues),
+                        plotOutput("TimeSeries1")
+                        #   )
+                        # )
+               ),
                tabPanel("WorldCloud",
-               				 fluidPage(
-                        sidebarLayout(
-                          sidebarPanel(
-                            textInput("text", label = h3("Text input"), 
-                            					value = "Termini da cercare")
+                        # fluidPage(
+                        #   sidebarLayout(
+                        sidebarPanel(
+                          textInput("text", label = h3("Text input"),
+                                    value = "Termini da cercare")
                         ),
                         mainPanel(
                           wordcloud2Output('wordcloud2'),
                           dataTableOutput("tableWords")
-                          )
                         )
-                      )
+                        #   )
+                        # )
                )
     )
+  )
 )
-)
-             
+
 
 
