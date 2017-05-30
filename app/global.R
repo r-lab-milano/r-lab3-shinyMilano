d5f1305d277d23e017351c8a826df81fcb4ca1d4
marchan001 <andrea.marchini@pirelli.com>
2017-05-30 22:51:31 +0200

    Read the file for the project root

--- app/global.R
+++ app/global.R
@@ -1,15 +1,11 @@
 library("shiny")
 library("shinydashboard")
 library("stringr")
-library("dplyr")
-library("wordcloud2")
 library("tidytext")
-library("readr")
-# library("scales")
-# library("dplyr")
-# library("tidyr")
-# library("stringr")
-library("ggplot2")
+library("tidyverse")
+library("wordcloud2")
+
+
 
 source("time_series_1.R")
 # source("renderSelect.R")
@@ -17,16 +13,9 @@ source("words.R")
 source("structure_plot.R")
 
 
-# load("../data/data_reshape.Rdata")
-
-#df <- as_tibble(datafin)
-
-df <- read_delim("~/R/shinyMilano/data/data_reshape.csv",
-                 ";", escape_double = FALSE, col_types = cols(stanziamento = col_number(),
-                                                              stanziamento_cassa = col_number()),
+df <- read_delim("../data/data_reshape.csv", ";", escape_double = FALSE, 
+								 col_types = cols(stanziamento = col_number(),
+								 								 stanziamento_cassa = col_number()),
                  trim_ws = TRUE)
+
 tipo <- unique(df$tipo)
-
-# tipo <- unique(df$TIPO)
-
-
