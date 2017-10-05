e88afa42a5658c51a0806a031ec4fffd24a31214
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2017-10-05 11:33:02 +0200

    fix bug: load necessary before source()s

--- app/global.R
+++ app/global.R
@@ -7,7 +7,21 @@ library("wordcloud2")
 library("sunburstR")
 library("d3r")
 
+df <- read_delim("./data/data_reshape.csv", ";", escape_double = FALSE,
+								 col_types = cols(stanziamento = col_number(),
+								 								 stanziamento_cassa = col_number()),
+                 trim_ws = TRUE)
 
+tipo <- unique(df$tipo)
+
+load("./data/data_reshape.Rdata")
+
+data_sun <- data.frame(grouping = paste0(datafin$ds_livello1,"-",
+																			datafin$ds_livello2,"-",
+																			datafin$ds_livello3,"-",
+																			datafin$ds_livello4),
+											 rendiconto = datafin$rendiconto,
+										year=datafin$anno)
 
 source("time_series.R")
 source("time_series_test.R")
@@ -16,21 +30,4 @@ source("time_series_test.R")
 source("wordcloud.R")
 source("structure_plot.R")
 #source("top10.R")
-
-
-df <- read_delim("./data/data_reshape.csv", ";", escape_double = FALSE,
-								 col_types = cols(stanziamento = col_number(),
-								 								 stanziamento_cassa = col_number()),
-								 trim_ws = TRUE)
-
-tipo <- unique(df$tipo)
-
-load("./data/data_reshape.Rdata")
-
-data_sun <- data.frame(grouping = paste0(datafin$ds_livello1,"-",
-																				 datafin$ds_livello2,"-",
-																				 datafin$ds_livello3,"-",
-																				 datafin$ds_livello4),
-											 rendiconto = datafin$rendiconto,
-											 year=datafin$anno)
 
